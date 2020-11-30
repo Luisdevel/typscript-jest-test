382c253fcd7d8ab58385abc39b1ef2a286524372
Luis Henrique <luishenrique.mf01@gmail.com>
2020-11-29 21:18:51 -0300

    configurando jest

---
+++ src/services/persistency.spec.ts
@@ -0,0 +1,13 @@
+describe('TESTANDO ALGUMA COISA', () => {
+  it('should return one', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+});
+
+describe('TESTANDO ALGUMA COISA', () => {
+  test('should retrn Luis', () => {
+    const name = 'Luis';
+    expect(name).toBe('Luis');
+  });
+});
